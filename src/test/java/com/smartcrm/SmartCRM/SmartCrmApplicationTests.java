63c83794837ebeec11e412f9678f5da286f96a9e
Affog7 <augustinaffognon2000@gmail.com>
2025-08-10 20:55:52 +0100

    first commit

---
+++ src/test/java/com/smartcrm/SmartCRM/SmartCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcrm.SmartCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
